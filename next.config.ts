83b04ca0dd91a2b8fab7edaf66c68b39211354f7
o.levytskyi <o.levytskyi@tsquad.net>
2026-05-18 21:13:31 +0300

    fix: apply basePath only in production

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/dev-cv',
+  basePath: isProd ? '/dev-cv' : '',
   images: {
     unoptimized: true,
   },
